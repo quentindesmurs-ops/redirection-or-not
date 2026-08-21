709ee2c8eaa9820cb27eb4c5cc31684684e9c0cb
quentindesmurs-ops <quentin.desmurs@uni-medias.com>
2026-08-21 14:07:19 +0200

    Update login.js

--- pages/pages/api/login.js
+++ pages/pages/api/login.js
@@ -1,13 +1,3 @@
-export default function handler(req, res) {
-  const { password } = req.body;
-
-  if (password === process.env.SITE_PASSWORD) {
-    res.setHeader(
-      "Set-Cookie",
-      `seo-tool-auth=ok; Path=/; HttpOnly; Max-Age=${60 * 60 * 24 * 30}`
-    );
-    return res.status(200).json({ success: true });
-  }
-
-  return res.status(401).json({ success: false });
+export default function Unused() {
+  return null;
 }
